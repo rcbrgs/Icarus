f62aa7425a8c12690d914ceb620e6ee1a77d0556
Renato Callado Borges <rborges@hush.com>
2016-06-15 23:08:21 +0200

    Added a few tables to the database, linked to the admin page.

--- IcarusWeb/Results_browser/admin.py
+++ IcarusWeb/Results_browser/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
-from .models import Algorithm, Classification, Result, Sample_input
+from .models import ( ACL,
+                      Algorithm,
+                      Classification,
+                      Donation,
+                      File,
+                      Image,
+                      Institution,
+                      Landmarks,
+                      Manual_classification,
+                      Procrustes,
+                      Result,
+                      Sample,
+                      Sample_image_wing,
+                      Sample_input,
+                      User,
+                      User_type )                      
 
 # Register your models here.
 
+admin.site.register ( ACL )
 admin.site.register ( Algorithm )
 admin.site.register ( Classification )
+admin.site.register ( Donation )
+admin.site.register ( File )
+admin.site.register ( Image )
+admin.site.register ( Institution )
+admin.site.register ( Landmarks )
+admin.site.register ( Manual_classification )
+admin.site.register ( Procrustes )
 admin.site.register ( Result )
+admin.site.register ( Sample )
+admin.site.register ( Sample_image_wing )
 admin.site.register ( Sample_input )
+admin.site.register ( User )
+admin.site.register ( User_type )
--- IcarusWeb/Results_browser/models.py
+++ IcarusWeb/Results_browser/models.py
@@ -2,6 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class User_type ( models.Model ):
+    name = models.CharField ( max_length = 255 )
+
+class Institution ( models.Model ):
+    name = models.CharField ( max_length = 255 )
+    
+class User ( models.Model ):
+    # must be before ACL
+    # must be after User_type
+    # must be after Institution
+    name = models.CharField ( max_length = 255 )
+    login = models.CharField ( max_length = 255 )
+    password = models.CharField ( max_length = 255 )
+    telephone = models.CharField ( max_length = 255 )
+    institution_id = models.ForeignKey ( Institution )
+    user_type_id = models.ForeignKey ( User_type )
+
+class ACL ( models.Model ):
+    # must be after User
+    read_permission = models.BooleanField ( )
+    write_permission = models.BooleanField ( )
+    users_list = models.ManyToManyField ( User )
+
 class Algorithm ( models.Model ):
     # Must be before Result.
     name = models.CharField ( max_length = 255 )
@@ -16,15 +39,42 @@ class Classification ( models.Model ):
     family = models.CharField ( max_length = 255 )
     subfamily = models.CharField ( max_length = 255 )
 
+class Donation ( models.Model ):
+    date = models.DateField ( )
+    number_of_structures = models.IntegerField ( )
+    donating_institution_id = models.ForeignKey ( Institution )
+    donor_user_id = models.ForeignKey ( User )
+    
+class File ( models.Model ):
+    host = models.CharField ( max_length = 255 )
+    path = models.CharField ( max_length = 255 )
+    ACL_id = models.ForeignKey ( ACL )
+    
 class Image ( models.Model ):
     image_height = models.IntegerField ( )
     image_width = models.IntegerField ( )
+
+class Procrustes ( models.Model ):
+    # must be before Landmarks
+    rotation_angle = models.FloatField ( )
+    planar_scaling_factor = models.FloatField ( )
+    
+class Landmarks ( models.Model ):
+    # must be after Procrustes
+    sequence_number = models.IntegerField ( )
+    horizontal_coordinates = models.FloatField ( )
+    vertical_coordinates = models.FloatField ( )
+    procrustes_id = models.ForeignKey ( Procrustes )
+
+class Manual_classification ( models.Model ):
+    user_id = models.ForeignKey ( User )
+    classification_id = models.ForeignKey ( Classification )
     
 class Sample_image_wing ( models.Model ):
     # must be before Sample_input
     image_id = models.ForeignKey ( Image, null = True, blank = False )
     #sample_wing_id = models.ForeignKey ( Sample_wing, null = True, blank = False )
-    #landmarks_id = models.ForeignKey ( Landmarks, null = True, blank = True )
+    landmarks_id = models.ForeignKey ( Landmarks, null = True, blank = True )
     #file_id = models.ForeignKey ( File, null = True, blank = False )
 
 class Sample_input ( models.Model ):
@@ -44,10 +94,10 @@ class Sample ( models.Model ):
     is_female_compatible = models.BooleanField ( )
     is_male_compatible = models.BooleanField ( )
     LECZ_registry = models.CharField ( max_length = 255 )
-#    #notes_file_id = models.ForeignKey (  )
-#    #collector_user_id = models.ForeignKey (  )
-#    #donation_id = models.ForeignKey (  )
-#    #manual_classification_id = models.ForeignKey (  )
+    #notes_file_id = models.ForeignKey (  )
+    collector_user_id = models.ForeignKey ( User )
+    donation_id = models.ForeignKey ( Donation )
+    manual_classification_id = models.ForeignKey ( Manual_classification )
 #    #mosquitolab_id = models.ForeignKey (  )
 #    #gene_bank_registry_id = models.ForeignKey (  )
 #    #phytophysiognomy_id = models.ForeignKey (  )
